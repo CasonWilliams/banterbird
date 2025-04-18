8d5cfa47c5ddc9838d51325eb1167a68e6ba06b8
CasonWilliams <cason.williams2@gmail.com>
2025-04-18 12:54:29 -0500

    Class
    Class 4/18/25

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/api/posts')
+def get_posts():
+    with open('post.json, 'r' ') as file:
+        post = json.load(file)
+        return jsonify(post)
 
 if __name__ == '__main__':
     app.run(debug=True)
