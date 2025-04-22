2237f5bb66d70b5a4ccd8631a199a3e6e64d3cc0
CasonWilliams <cason.williams2@gmail.com>
2025-04-22 10:41:05 -0500

    Post
    Adding in posting on the site

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import json
 
 app = Flask(__name__)
@@ -9,9 +9,24 @@ def index():
 
 @app.route('/api/posts')
 def get_posts():
-    with open('post.json, 'r' ') as file:
+    with open('posts.json', 'r') as file:
         post = json.load(file)
         return jsonify(post)
+
+@app.route('/api/posts', methods=['POST'] )
+def add_post():
+    new_post = request.get_json()
+
+    with open('posts.json', 'r') as file:
+        posts = json.load(file)
+
+    posts.insert(0, new_post)
+
+    with open('posts.json', 'w') as file:
+        json.dump(posts, file, indent=4)
+
+    return jsonify({"status": "success"}), 201
+
 
 if __name__ == '__main__':
     app.run(debug=True)
